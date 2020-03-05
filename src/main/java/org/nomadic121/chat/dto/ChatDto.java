2cd3891cc80ff4d7602b228651e84744d458902b
nomadic121 <nomadic121@gmail.com>
2020-03-05 04:26:51 +0300

    fix ChatDto

--- src/main/java/org/nomadic121/chat/dto/ChatDto.java
+++ src/main/java/org/nomadic121/chat/dto/ChatDto.java
@@ -1,5 +1,12 @@
 package org.nomadic121.chat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatDto {
 
     private String title;
