87bdb0bfa3e7f10109c1f58f0eb8b9aa4bd637a1
nomadic121 <nomadic121@gmail.com>
2020-03-06 07:22:17 +0300

    Securing a Web Application

--- src/main/java/org/nomadic121/chat/controller/View/ChatController.java
+++ src/main/java/org/nomadic121/chat/controller/View/ChatController.java
@@ -2,11 +2,9 @@ package org.nomadic121.chat.controller.View;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.nomadic121.chat.form.UserForm;
 import org.nomadic121.chat.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -17,9 +15,8 @@ public class ChatController {
     private final @NonNull UserService userService;
 
     @GetMapping("/chatUI")
-    public String getChat(@RequestParam(name = "name", required = false, defaultValue = "") String name, Map<String, Object> model) {
-        userService.save(new UserForm(name, ""));
-        return "index.html";
+    public String getChat(Map<String, Object> model) {
+        return "chatUI.html";
     }
 
 }
