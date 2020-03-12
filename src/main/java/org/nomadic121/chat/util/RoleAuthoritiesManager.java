cdca53925bdade33a93a0ba7b6410552448eec41
nomadic121 <nomadic121@gmail.com>
2020-03-12 21:19:11 +0300

    create RoleAuthoritiesManager

---
+++ src/main/java/org/nomadic121/chat/util/RoleAuthoritiesManager.java
@@ -0,0 +1,47 @@
+package org.nomadic121.chat.util;
+
+import org.nomadic121.chat.entity.Role;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum RoleAuthoritiesManager {
+
+    USER(Role.ROLE_USER),
+    ADMINISTRATOR(Role.ROLE_ADMINISTRATOR),
+    MODERATOR(Role.ROLE_MODERATOR),
+    BANNED(Role.BANNED);
+
+    public Set<Role> getAuthorities() {
+        return authorities;
+    }
+
+    private final Set<Role> authorities = new HashSet<>();
+
+    private RoleAuthoritiesManager(Role role) {
+        switch (role) {
+            case BANNED:
+                authorities.add(Role.ROLE_USER);
+                authorities.add(Role.CAN_GET_MESSAGES);
+                authorities.add(Role.BANNED);
+                break;
+            case ROLE_ADMINISTRATOR:
+                authorities.add(Role.ROLE_ADMINISTRATOR);
+                authorities.add(Role.CAN_GET_MESSAGES);
+                authorities.add(Role.CAN_SEND_MESSAGES);
+                break;
+            case ROLE_MODERATOR:
+                authorities.add(Role.ROLE_MODERATOR);
+                authorities.add(Role.CAN_EDIT_AND_DELETE_MESSAGES);
+                authorities.add(Role.CAN_GET_MESSAGES);
+                authorities.add(Role.CAN_SEND_MESSAGES);
+                break;
+            case ROLE_USER:
+                authorities.add(Role.ROLE_USER);
+                authorities.add(Role.CAN_GET_MESSAGES);
+                authorities.add(Role.CAN_SEND_MESSAGES);
+                break;
+        }
+    }
+
+}
