34f2ec74a7b5c38c12f8321be429f8a1c9e79982
nomadic121 <nomadic121@gmail.com>
2020-03-12 19:36:23 +0300

    add PreAuthorize and refactor

--- src/main/java/org/nomadic121/chat/controller/REST/MessageController.java
+++ src/main/java/org/nomadic121/chat/controller/REST/MessageController.java
@@ -6,6 +6,7 @@ import org.nomadic121.chat.dto.MessageDto;
 import org.nomadic121.chat.form.MessageForm;
 import org.nomadic121.chat.service.MessageService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,82 +19,73 @@ public class MessageController {
 
     private final @NonNull MessageService messageService;
 
+    @PreAuthorize(value = "hasAuthority('CAN_GET_MESSAGES')")
     @GetMapping("/messages")
-    public List<MessageDto> getMessageList() {
-        return messageService.getAllMessages();
+    public ResponseEntity<List<MessageDto>> getMessageList() {
+        return ResponseEntity.ok(messageService.getAllMessages());
     }
 
+    @PreAuthorize(value = "hasAuthority('CAN_GET_MESSAGES')")
     @GetMapping("/messages/{chatId}")
-    public List<MessageDto> getMessageListByChatId(@PathVariable(name = "chatId", required = true) String chatId) {
-        if (!chatId.isEmpty()) {
+    public ResponseEntity<List<MessageDto>> getMessageListByChatId(@PathVariable(name = "chatId", required = true) String chatId) {
+        try {
             Long id = Long.valueOf(chatId);
-            try {
-                return messageService.getMessagesByChatId(id);
-            } catch (EntityNotFoundException e) {
-                return null;
-            }
+            return ResponseEntity.ok(messageService.getMessagesByChatId(id));
+        } catch (EntityNotFoundException | NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
         }
-        return null;
     }
 
+    @PreAuthorize(value = "hasAuthority('CAN_GET_MESSAGES')")
     @GetMapping("/message/{messageId}")
-    public MessageDto getMessagesById(@PathVariable(name = "messageId", required = true) String messageId) {
-        if (!messageId.isEmpty()) {
+    public ResponseEntity<MessageDto> getMessagesById(@PathVariable(name = "messageId", required = true) String messageId) {
+        try {
             Long id = Long.valueOf(messageId);
-            try {
-                return messageService.getOneById(id);
-            } catch (EntityNotFoundException e) {
-                return null;
-            }
+            return ResponseEntity.ok(messageService.getOneById(id));
+        } catch (EntityNotFoundException | NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
         }
-        return null;
     }
 
+    @PreAuthorize(value = "hasAuthority('CAN_SEND_MESSAGES')")
     @PostMapping("/messages/{chatId}")
     public ResponseEntity<Void> postMessageByChatId(@PathVariable(name = "chatId", required = true) String chatId,
-                                                  Authentication authentication,
-                                                  @RequestBody MessageForm messageForm) {
-        if (!chatId.isEmpty()) {
+                                                    Authentication authentication,
+                                                    @RequestBody MessageForm messageForm) {
+        try {
             Long id = Long.valueOf(chatId);
-            try {
-                messageService.saveAndDeliverMessage(authentication, id, messageForm);
-                return ResponseEntity.ok().build();
-            } catch (EntityNotFoundException e) {
-                return ResponseEntity.badRequest().build();
-            }
+            messageService.saveAndDeliverMessage(authentication, id, messageForm);
+            return ResponseEntity.ok().build();
+        } catch (EntityNotFoundException | NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.badRequest().build();
     }
 
+    @PreAuthorize(value = "hasAuthority('CAN_EDIT_AND_DELETE_MESSAGES')")
     @DeleteMapping("/messages/{messageId}")
     public ResponseEntity<Void> deleteMessageById(@PathVariable(name = "messageId", required = true) String messageId,
                                                   Authentication authentication) {
-        if (!messageId.isEmpty()) {
+        try {
             Long id = Long.valueOf(messageId);
-            try {
-                messageService.deleteMessageById(authentication, id);
-                return ResponseEntity.ok().build();
-            } catch (EntityNotFoundException e) {
-                return ResponseEntity.badRequest().build();
-            }
+            messageService.deleteMessageById(authentication, id);
+            return ResponseEntity.ok().build();
+        } catch (EntityNotFoundException | NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.badRequest().build();
     }
 
+    @PreAuthorize(value = "hasAuthority('CAN_EDIT_AND_DELETE_MESSAGES')")
     @PutMapping("/messages/{messageId}")
     public ResponseEntity<Void> updateMessageById(@PathVariable(name = "messageId", required = true) String messageId,
                                                   Authentication authentication,
                                                   @RequestBody MessageForm messageForm) {
-        if (!messageId.isEmpty()) {
+        try {
             Long id = Long.valueOf(messageId);
-            try {
-                messageService.updateMessageById(authentication, id, messageForm);
-                return ResponseEntity.ok().build();
-            } catch (EntityNotFoundException e) {
-                return ResponseEntity.badRequest().build();
-            }
+            messageService.updateMessageById(authentication, id, messageForm);
+            return ResponseEntity.ok().build();
+        } catch (EntityNotFoundException | NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.badRequest().build();
     }
 
 }
